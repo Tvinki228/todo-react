48e2aa9933a70b9fd43f8c7907de230f127f3921
Tvinki228 <pes-log@ukr.net>
2024-11-12 14:19:34 +0100

    Add basic header and basic syles

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Header } from "./components/Header/Header";
+
+const TodoList = () => {
+    const todos = ["My first todo", "My second todo"];
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <li>
+                    <input type="checkbox" /> {todo}
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 function App() {
-    return <>Hello Losers!</>;
+    return (
+        <>
+            <Header />
+            <TodoList />
+        </>
+    );
 }
 
 export default App;
